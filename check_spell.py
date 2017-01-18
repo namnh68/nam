600fc1d892a3fc6cdc5bb0a9eb7abd35d4c99a8e
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-01-18 16:48:15 +0700

    Update

--- check_spell.py
+++ check_spell.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # Author: Nguyen Hoai Nam
 
 import commands
@@ -6,11 +8,10 @@ import sys
 
 
 def filter_spell(path_file, extensions):
-    for ext in extensions:
-        command = 'find %s -name "*.%s"| xargs cat | aspell --list | sort -u' % (path_file, ext)
-        typos = commands.getstatusoutput(command)[1]
-        typos_list = typos.split('\n')
-        typos_list += typos_list
+    command = 'find %s -name "*.%s"| xargs cat | aspell --list | sort -u' % (path_file, extensions)
+    typos = commands.getstatusoutput(command)[1]
+    typos_list = typos.split('\n')
+    typos_list += typos_list
     return typos_list
 
 
@@ -22,7 +23,7 @@ def check_spell_again(list_word, current_folder):
         command = 'grep -ron "%s" | grep -v "Binary" | wc -l' % word
         count = commands.getstatusoutput(command)[1]
         if len(count) >= 1:
-            if int(count) >= 5:
+            if int(count) > 1:
                 pass
             else:
                 list_word_finally.append(word)
@@ -34,13 +35,13 @@ def check_spell_again(list_word, current_folder):
 def main():
     if len(sys.argv) == 1:
         path_file = os.getcwd()
-        extension = ['py']
+        extension = 'py'
     elif len(sys.argv) == 2:
-        path_file = sys.argv[1]
-        extension = ['py']
-    else:
-        path_file = sys.argv[1]
-        extension = sys.argv[2:]
+        extension = sys.argv[1]
+        path_file = os.getcwd()
+    elif len(sys.argv) == 3:
+        path_file = sys.argv[2]
+        extension = sys.argv[1]
     list_spell = filter_spell(path_file, extension)
     out_put = check_spell_again(list_spell, path_file)
     print out_put
@@ -48,14 +49,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
 
