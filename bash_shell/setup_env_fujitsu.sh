30d8f4e94d4cc3fa3f1f385a3e3b8acd3b567d54
Nguyen Hoai Nam <namptit307@gmail.com>
2017-03-13 09:18:10 +0700

    Update setup_env_fujitsu.sh

--- bash_shell/setup_env_fujitsu.sh
+++ bash_shell/setup_env_fujitsu.sh
@@ -23,24 +23,24 @@ iface eth0 inet static
 address $IP_EXT
 netmask 255.255.255.192
 gateway 10.164.180.65
-dns-nameservers 10.164.180.65
+dns-nameservers 10.164.180.65 10.0.238.70 10.164.177.145
 EOF
 
 if [ -f $apt_file ] ; then
 rm $apt_file
 fi
 cat << EOF > $apt_file
-Acquire::http::proxy "http://10.164.177.168:8080/";
-Acquire::https::proxy "https://10.164.177.168:8080/";
-Acquire::ftp::proxy "ftp://10.164.177.168:8080/";
-Acquire::socks::proxy "socks://10.164.177.168:8080/";
+Acquire::http::proxy "http://10.164.177.170:8080/";
+Acquire::https::proxy "https://10.164.177.170:8080/";
+Acquire::ftp::proxy "ftp://10.164.177.170:8080/";
+Acquire::socks::proxy "socks://10.164.177.170:8080/";
 EOF
 
 rm $env_file
 cat << EOF > $env_file
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-http_proxy="http://10.164.177.168:8080/"
-https_proxy="https://10.164.177.168:8080/"
+http_proxy="http://10.164.177.170:8080/"
+https_proxy="https://10.164.177.170:8080/"
 no_proxy=localhost,10.10.10.0/24,10.10.20.0/24,10.164.180.65/25,164.254.169.254
 EOF
 
