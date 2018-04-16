ac18f80971b8d3b97f09f632da21042a7663eb09
Nguyen Hoai Nam <namptit307@gmail.com>
2018-04-17 00:02:29 +0700

    Update examples for thread and process

---
+++ python-example/thread_process/multithreading-worker.py
@@ -0,0 +1,18 @@
+import random
+import threading
+import time
+
+results = []
+
+
+def compute():
+    results.append(sum(
+        [random.randint(1, 100) for i in range(1000000)]))
+
+workers = [threading.Thread(target=compute) for x in range(8)]
+for worker in workers:
+    worker.start()
+for worker in workers:
+    worker.join()
+
+print("Results: %s" % results)
